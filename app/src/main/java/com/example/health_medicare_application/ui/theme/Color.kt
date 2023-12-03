cfc10f0a7b82f69dfb5eae2c926663bd3cabd5c6
Ruchitha <ganderuchitha@gmail.com>
2023-12-03 01:50:38 -0600

    Reuploading files

---
+++ app/src/main/java/com/example/health_medicare_application/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.health_medicare_application.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val purple673 = Color(0xFF68BFCF)
+val purewhite = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val yellow = Color(0xFFDAC720)
+
